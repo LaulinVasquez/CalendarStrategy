6737f5de50e267f5f2942a82a626fe9d506465c3
Laurin Vasquez <113741390+LaulinVasquez@users.noreply.github.com>
2026-03-20 01:05:28 -0600

    calendar functions, and rendering

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,2 +1,23 @@
 // connects everything
 
+let currentDate = new Date();
+
+function updateCalendar() {
+  renderCalendar(
+    currentDate.getMonth(),
+    currentDate.getFullYear()
+  );
+}
+
+document.getElementById("prevBtn").addEventListener("click", () => {
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  updateCalendar();
+});
+
+document.getElementById("nextBtn").addEventListener("click", () => {
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  updateCalendar();
+});
+
+// Initial load
+updateCalendar();
--- frontend/js/calendar.js
+++ frontend/js/calendar.js
@@ -1,2 +1,31 @@
 // builds the calendar UI
 
+function renderCalendar(month, year) {
+    const grid = document.getElementById("calendarGrid");
+    const monthYear = document.getElementById("monthYear");
+
+    grid.innerHTML = ""
+
+    const firstDay = getFirstDayOfMonth(year,month);
+    const totalDays = getDaysInMonth(year,month);
+    const monthNames = [
+            "January","February","March","April","May","June","July","August","September","October","November","December"
+    ];
+    monthYear.textContent = `${monthNames[month]} - ${year}`;
+
+    for (let i = 0; i < firstDay; i++){
+        const empty = document.createElement("div");
+        grid.appendChild(empty)
+    }
+    for (let day=1; day <= totalDays; day++){
+        const cell = document.createElement("div");
+        cell.textContent = day;
+       
+        if (isToday(day,month,year)) {
+            cell.classList.add("today");
+        }
+        grid.appendChild(cell);
+    }
+    
+}
+
